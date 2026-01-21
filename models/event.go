24bd4255978380ad340a1a27ac2fd7c9d71b7662
Norie <norie.macewan@tiscali.co.uk>
2026-01-21 22:38:42 +0000

    Added eventUpdate route

--- models/event.go
+++ models/event.go
@@ -56,12 +56,12 @@ func GetAllEvents() ([]Event, error) {
 	defer rows.Close()
 
 	var events []Event
-	
+
 	for rows.Next() {
 		var event Event
-		err:=rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
 
-		if err!=nil {
+		if err != nil {
 			return nil, err
 		}
 
@@ -73,17 +73,41 @@ func GetAllEvents() ([]Event, error) {
 
 func GetEventByID(id int64) (*Event, error) {
 
-	query:= "SELECT * FROM events WHERE id = ?"
+	query := "SELECT * FROM events WHERE id = ?"
 
-	row :=	db.DB.QueryRow(query, id)
+	row := db.DB.QueryRow(query, id)
 
 	var event Event
 	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
 
 	if err != nil {
-		return  nil, err
+		return nil, err
 	}
 
 	return &event, nil
+
+}
+
+func (e Event) Update() error {
+	// add event to database
+	query := `
+		UPDATE events 
+		SET name = ?, description = ?, location = ?, dateTime = ?, user_id = ?
+		WHERE id = ?`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()	
 	
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID, e.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return err
 }
